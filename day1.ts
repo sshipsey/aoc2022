b142d480958e25095c2ab5a37051a5ccdb846821
Steve Shipsey <sashipsey@gmail.com>
2022-12-01 02:52:23 -0500

    day 1

---
+++ day1.ts
@@ -0,0 +1,16 @@
+import { readFile } from './lib/readFile';
+
+const p1 = (cals: number[]) => Math.max(...cals);
+
+const p2 = (cals: number[]) => {
+  const sortedCals = cals.sort((a, b) => b - a);
+  return sortedCals[0] + sortedCals[1] + sortedCals[2];
+}
+
+(async () => {
+  const elves = (await readFile('./inputs/day1')).split('\r\n\r\n');
+  const calories = elves.map(elf => elf.split('\r\n')
+    .map(Number)
+    .reduce((a, b) => a + b, 0));
+  console.log(p1(calories), p2(calories));
+})();
---
+++ lib/readFile.ts
@@ -0,0 +1,4 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export const readFile = (file: string) => new Promise<string>(resolve  => fs.readFile(path.join(__dirname, `../${file}`), 'utf-8', (_, data) => resolve(data)));
