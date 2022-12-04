9c4d8a518385c8dc0ef5eda70605170ddf84c9e1
Steve Shipsey <sashipsey@gmail.com>
2022-12-04 02:00:41 -0500

    file formatting

--- lib/readFile.ts
+++ lib/readFile.ts
@@ -1,4 +1,9 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export const readFile = (file: string) => new Promise<string>(resolve  => fs.readFile(path.join(__dirname, `../${file}`), 'utf-8', (_, data) => resolve(data)));
+export const readFile = (file: string) =>
+  new Promise<string>((resolve) =>
+    fs.readFile(path.join(__dirname, `../${file}`), 'utf-8', (_, data) =>
+      resolve(data)
+    )
+  );
